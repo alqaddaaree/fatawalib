bd894bb94a2bf8b2d51ba632b5085a873be3e6f7
Abderrahmene Mihoub <abderrahmene_mhb@proton.me>
2026-05-28 19:23:22 +0100

    switched to pagescms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
-import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   site: 'https://fatawalib.com',
   adapter: vercel(),
-  integrations: [react(), tailwind(), sitemap(), keystatic()],
+  integrations: [react(), tailwind(), sitemap()],
   output: 'hybrid',
   content: {
     dir: './src/content',
